df05f82f6effe2d96c8af42b1a845731664d4f3f
Rodrigo Dezouzart <rodrigodezouzart@gmail.com>
2023-06-27 17:21:21 +0100

    adding cache logic to fetch image

--- TheDogApp/TheDogApp/Helpers/UIImageViews.swift
+++ TheDogApp/TheDogApp/Helpers/UIImageViews.swift
@@ -1,32 +1,57 @@
 import UIKit
 
+class ImageCache {
+    static let shared = ImageCache()
+    let cache = NSCache<NSString, UIImage>()
+    
+    private init() {}
+}
+
 extension UIImageView {
     func load(from url: URL, completion: @escaping () -> Void) {
         DispatchQueue.global().async {
+            if let cachedImage =  ImageCache.shared.cache.object(forKey: url.absoluteString as NSString) {
+                self.load(cachedImage, with: completion)
+                return
+            }
+            
             var request = URLRequest(url: url)
             BreedEndpoints.setCredentialHeader(request: &request)
             
-            let task = SystemRemoteSession().dataTask(request: request) { [weak self] (data, response, error) in
-                guard let context = self, let data = data, error == nil else {
-                    DispatchQueue.main.async {
-                        completion()
-                    }
-                    return
-                }
-                
+            let task = self.createTask(request, completion: completion)
+            
+            task?.resume()
+        }
+    }
+    
+    private func createTask(_ request: URLRequest, completion: @escaping () -> Void) -> RemoteSessionDataTask? {
+        let task = SystemRemoteSession().dataTask(request: request) { [weak self] (data, response, error) in
+            guard let context = self, let data = data, error == nil, let image = UIImage(data: data) else {
                 DispatchQueue.main.async {
                     completion()
-                    let image = UIImage(data: data)
-                    UIView.transition(
-                        with: context,
-                        duration: 0.5,
-                        options: .transitionCrossDissolve,
-                        animations: { context.image = image },
-                        completion: nil
-                    )
                 }
+                return
             }
-            task?.resume()
+            
+            if let url = request.url  {
+                ImageCache.shared.cache.setObject(image, forKey: url.absoluteString as NSString)
+            }
+            context.load(image, with: completion)
+        }
+        
+        return task
+    }
+    
+    private func load(_ image: UIImage, with completion: @escaping () -> Void) {
+        DispatchQueue.main.async {
+            completion()
+            UIView.transition(
+                with: self,
+                duration: 0.3,
+                options: .transitionCrossDissolve,
+                animations: { self.image = image },
+                completion: nil
+            )
         }
     }
 }
